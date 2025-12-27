8a02be2c2a854db6a9d8130e149f62579356e3a3
hkaku <public@example.com>
2025-12-27 20:12:22 +0900

    Fix: debian - 2025-12-27 20:12:21

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- ZFVimIM 词库路径已移至 plugins/zfvimim.lua 中配置
+-- sbzrvimim 词库路径配置在 plugins/sbzr.vimi.m.lua 中
