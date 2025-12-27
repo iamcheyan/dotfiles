0b8b5479cab8c742b1dcb510387553279b2779f1
hkaku <public@example.com>
2025-12-28 00:26:34 +0900

    20251228

--- plugins/zinit/zinit.zsh
+++ plugins/zinit/zinit.zsh
@@ -5,5 +5,12 @@ fi
 
 source ~/.zinit/bin/zinit.zsh
 
-alias zz='zoxide query -i'
+# zz 函数：交互式 zoxide 目录查询并切换
+function zz() {
+  local dir
+  dir=$(zoxide query -i)
+  if [[ -n "$dir" ]]; then
+    cd "$dir"
+  fi
+}
 
