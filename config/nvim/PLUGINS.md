ef650f87bf36d5c902738280ac49fbbeeb988269
TETSUYA <tetusya.ueno@outlook.com>
2026-04-24 17:49:33 +0900

    20260424_174933

--- config/nvim/PLUGINS.md
+++ config/nvim/PLUGINS.md
@@ -43,6 +43,7 @@
 *   **telescope.nvim**: 强大的模糊搜索器 (搜索文件、grep、git 等)。
 *   **plenary.nvim**: Lua 实用函数库 (Telescope 等插件的依赖)。
 *   **flash.nvim**: 极速光标跳转插件 (类似 Easymotion/Sneak)。
+*   **oil.nvim**: 把目录当成可编辑 buffer 的文件管理器，适合批量新建、重命名、删除文件。
 *   **trouble.nvim**: 漂亮的诊断、引用和快速修复列表。
 *   **todo-comments.nvim**: 高亮并列出代码中的 TODO, FIXME, HACK 等注释。
 *   **grug-far.nvim**: 全局搜索和替换工具 (基于 ripgrep)。
---
+++ config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      {
+        "-",
+        "<cmd>Oil<cr>",
+        desc = "Open parent directory (Oil)",
+      },
+      -- {
+      --   "<leader>o",
+      --   "<cmd>Oil<cr>",
+      --   desc = "Open Oil",
+      -- },
+      {
+        "<leader>o",
+        "<cmd>Oil --float<cr>",
+        desc = "Open Oil (float)",
+      },
+    },
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+      },
+      delete_to_trash = false,
+      skip_confirm_for_simple_edits = false,
+      constrain_cursor = "editable",
+      view_options = {
+        show_hidden = false,
+        natural_order = true,
+      },
+      float = {
+        padding = 2,
+        max_width = 0.8,
+        max_height = 0.8,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
+        },
+      },
+      keymaps = {
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.tcd", mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+      },
+      use_default_keymaps = false,
+    },
+  },
+}
