07492a64823e2926acdf2cc6778ed7b9598e1645
iamcheyan <me@iamcheyan.com>
2026-04-24 17:06:11 +0900

    20260424_170611

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -4,84 +4,276 @@ return {
     opts = {
       highlights = {
         fill = {
-          bg = "#111111",
-          fg = "#4a4a4a",
+          bg = "#161616",
+          fg = "#5e5e5e",
         },
-        background = {
-          bg = "#1b1b1b",
-          fg = "#707070",
+        trunc_marker = {
+          bg = "#1c1c1c",
+          fg = "#6a6a6a",
         },
-        buffer = {
-          bg = "#1b1b1b",
-          fg = "#707070",
+        group_separator = {
+          bg = "#1c1c1c",
+          fg = "#1c1c1c",
         },
-        buffer_visible = {
-          bg = "#242424",
-          fg = "#9a9a9a",
-        },
-        buffer_selected = {
-          bg = "#3a3a3a",
-          fg = "#e2e2e2",
-          bold = true,
-        },
-        numbers = {
-          bg = "#1b1b1b",
-          fg = "#5f5f5f",
-        },
-        numbers_visible = {
-          bg = "#242424",
+        group_label = {
+          bg = "#1c1c1c",
           fg = "#8a8a8a",
         },
-        numbers_selected = {
-          bg = "#3a3a3a",
+        tab = {
+          bg = "#1c1c1c",
+          fg = "#7a7a7a",
+        },
+        tab_selected = {
+          bg = "#363636",
+          fg = "#efefef",
+          bold = true,
+        },
+        tab_close = {
+          bg = "#1c1c1c",
+          fg = "#7a7a7a",
+        },
+        background = {
+          bg = "#1c1c1c",
+          fg = "#7a7a7a",
+        },
+        buffer = {
+          bg = "#1c1c1c",
+          fg = "#7a7a7a",
+        },
+        buffer_visible = {
+          bg = "#262626",
+          fg = "#a0a0a0",
+        },
+        buffer_selected = {
+          bg = "#363636",
+          fg = "#efefef",
+          bold = true,
+        },
+        diagnostic = {
+          bg = "#1c1c1c",
+          fg = "#7a7a7a",
+        },
+        diagnostic_visible = {
+          bg = "#262626",
+          fg = "#a0a0a0",
+        },
+        diagnostic_selected = {
+          bg = "#363636",
+          fg = "#e0e0e0",
+          bold = true,
+        },
+        error = {
+          bg = "#1c1c1c",
+          fg = "#c75c6a",
+        },
+        error_visible = {
+          bg = "#262626",
+          fg = "#d46b79",
+        },
+        error_selected = {
+          bg = "#363636",
+          fg = "#f07a88",
+          bold = true,
+        },
+        error_diagnostic = {
+          bg = "#1c1c1c",
+          fg = "#c75c6a",
+        },
+        error_diagnostic_visible = {
+          bg = "#262626",
+          fg = "#d46b79",
+        },
+        error_diagnostic_selected = {
+          bg = "#363636",
+          fg = "#f07a88",
+          bold = true,
+        },
+        warning = {
+          bg = "#1c1c1c",
+          fg = "#b89a58",
+        },
+        warning_visible = {
+          bg = "#262626",
+          fg = "#c8ab68",
+        },
+        warning_selected = {
+          bg = "#363636",
+          fg = "#e0c078",
+          bold = true,
+        },
+        warning_diagnostic = {
+          bg = "#1c1c1c",
+          fg = "#b89a58",
+        },
+        warning_diagnostic_visible = {
+          bg = "#262626",
+          fg = "#c8ab68",
+        },
+        warning_diagnostic_selected = {
+          bg = "#363636",
+          fg = "#e0c078",
+          bold = true,
+        },
+        info = {
+          bg = "#1c1c1c",
+          fg = "#6f8fa8",
+        },
+        info_visible = {
+          bg = "#262626",
+          fg = "#80a1bb",
+        },
+        info_selected = {
+          bg = "#363636",
+          fg = "#9fc0db",
+          bold = true,
+        },
+        info_diagnostic = {
+          bg = "#1c1c1c",
+          fg = "#6f8fa8",
+        },
+        info_diagnostic_visible = {
+          bg = "#262626",
+          fg = "#80a1bb",
+        },
+        info_diagnostic_selected = {
+          bg = "#363636",
+          fg = "#9fc0db",
+          bold = true,
+        },
+        hint = {
+          bg = "#1c1c1c",
+          fg = "#6d9a7b",
+        },
+        hint_visible = {
+          bg = "#262626",
+          fg = "#7fad8d",
+        },
+        hint_selected = {
+          bg = "#363636",
+          fg = "#9fcbb0",
+          bold = true,
+        },
+        hint_diagnostic = {
+          bg = "#1c1c1c",
+          fg = "#6d9a7b",
+        },
+        hint_diagnostic_visible = {
+          bg = "#262626",
+          fg = "#7fad8d",
+        },
+        hint_diagnostic_selected = {
+          bg = "#363636",
+          fg = "#9fcbb0",
+          bold = true,
+        },
+        duplicate = {
+          bg = "#1c1c1c",
+          fg = "#7f7f7f",
+          italic = false,
+        },
+        duplicate_visible = {
+          bg = "#262626",
+          fg = "#a0a0a0",
+          italic = false,
+        },
+        duplicate_selected = {
+          bg = "#363636",
           fg = "#d8d8d8",
+          italic = false,
+        },
+        numbers = {
+          bg = "#1c1c1c",
+          fg = "#666666",
+        },
+        numbers_visible = {
+          bg = "#262626",
+          fg = "#929292",
+        },
+        numbers_selected = {
+          bg = "#363636",
+          fg = "#dedede",
           bold = true,
         },
         close_button = {
-          bg = "#1b1b1b",
-          fg = "#666666",
+          bg = "#1c1c1c",
+          fg = "#707070",
         },
         close_button_visible = {
-          bg = "#242424",
-          fg = "#8a8a8a",
+          bg = "#262626",
+          fg = "#949494",
         },
         close_button_selected = {
-          bg = "#3a3a3a",
+          bg = "#363636",
           fg = "#cfcfcf",
         },
         modified = {
-          bg = "#1b1b1b",
-          fg = "#8a8a8a",
+          bg = "#1c1c1c",
+          fg = "#8e8e8e",
         },
         modified_visible = {
-          bg = "#242424",
-          fg = "#adadad",
+          bg = "#262626",
+          fg = "#b3b3b3",
         },
         modified_selected = {
-          bg = "#3a3a3a",
+          bg = "#363636",
           fg = "#f0f0f0",
         },
         separator = {
-          bg = "#111111",
-          fg = "#111111",
+          bg = "#1c1c1c",
+          fg = "#1c1c1c",
         },
         separator_visible = {
-          bg = "#111111",
-          fg = "#111111",
+          bg = "#262626",
+          fg = "#262626",
         },
         separator_selected = {
-          bg = "#111111",
-          fg = "#111111",
+          bg = "#363636",
+          fg = "#363636",
+        },
+        tab_separator = {
+          bg = "#1c1c1c",
+          fg = "#1c1c1c",
+        },
+        tab_separator_selected = {
+          bg = "#363636",
+          fg = "#363636",
         },
         indicator_selected = {
-          bg = "#3a3a3a",
-          fg = "#3a3a3a",
+          bg = "#363636",
+          fg = "#363636",
+        },
+        indicator_visible = {
+          bg = "#262626",
+          fg = "#262626",
+        },
+        pick = {
+          bg = "#1c1c1c",
+          fg = "#d0d0d0",
+          bold = true,
+          italic = false,
+        },
+        pick_visible = {
+          bg = "#262626",
+          fg = "#dcdcdc",
+          bold = true,
+          italic = false,
+        },
+        pick_selected = {
+          bg = "#363636",
+          fg = "#ffffff",
+          bold = true,
+          italic = false,
+        },
+        offset_separator = {
+          bg = "#161616",
+          fg = "#161616",
         },
       },
       options = {
         mode = "buffers",
         diagnostics = false,
         themable = false,
+        color_icons = false,
         offsets = {
           {
             filetype = "snacks_layout_box",
