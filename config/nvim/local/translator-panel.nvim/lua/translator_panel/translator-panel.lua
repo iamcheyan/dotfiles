531f145f13274881a1eb852d33c1f16604bab00a
ho.kaku <ho.kaku@sbilife.co.jp>
2026-06-02 14:39:01 +0900

    add:vifm

--- config/nvim/local/translator-panel.nvim/lua/translator_panel/translator-panel.lua
+++
@@ -1,9 +0,0 @@
--- ~/dotfiles/config/nvim/lua/plugins/translator-panel.lu
-
-return {
-	dir = vim.fn.expand("~/.local/share/nvim/lazy/translator-panel.nvim"),
-	name = "translator-panel.nvim",
-	config = function()
-		require("translator_panel").setup()
-	end,
-}
