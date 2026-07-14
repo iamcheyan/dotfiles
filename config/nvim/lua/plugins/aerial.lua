8dd44d2f903c068553d533bbe7f9918bd0a80d5c
iamcheyan <me@iamcheyan.com>
2026-07-14 09:06:23 +0900

    refactor:vim

--- config/nvim/lua/plugins/aerial.lua
+++ config/nvim/lua/plugins/aerial.lua
@@ -8,21 +8,14 @@ return {
       layout = {
         default_direction = "left",
         placement = "window",
-        width = 30, -- 增加默认宽度以显示更多内容
-        max_width = { 120, 0.4 }, -- 最大宽度为120列或窗口宽度的40%
-        resize_to_content = true, -- 根据内容自动调整大小
+        width = 30,
+        max_width = { 120, 0.4 },
+        resize_to_content = true,
       },
       show_guides = true,
       manage_folds = true,
       link_tree_to_folds = true,
       link_tree_to_window = true,
-    },
-  },
-  {
-    "folke/trouble.nvim",
-    optional = true,
-    keys = {
-      { "<leader>cs", false },
     },
   },
 }
